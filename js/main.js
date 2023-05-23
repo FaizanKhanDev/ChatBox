de14c5840d8f97f8ef47bdb2e6a007f55f7d2147
FaizanKhanDev <faizan480khan@gmail.com>
2023-05-23 07:56:59 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// === Chat Box === //
+const formWrapper = document.querySelector(".formbold-form-wrapper");
+const formActionButton = document.querySelector(".formbold-action-btn");
+function chatboxToogleHandler() {
+    formWrapper.classList.toggle("active");
+    formActionButton.classList.toggle("active");
+}
+// === Chat Box === //
